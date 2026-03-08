82f11cffceb34f6039b9c3fb5182ad1fd00b33a2
Marcos Vinicius <mvap.marcos@gmail.com>
2026-03-08 19:31:11 -0300

    Comitt01

---
+++ src/br/senac/ads/service/ClienteService.java
@@ -0,0 +1,60 @@
+package br.senac.ads.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import br.senac.ads.model.Cliente;
+
+public class ClienteService {
+
+	List<Cliente> clientes = new ArrayList<Cliente>();
+	
+	public void adicionarCliente(int id, String nome, String telefone) {
+		Cliente c = new Cliente(id, nome, telefone);
+		clientes.add(c);
+		
+		System.out.println("Cliente adicionado com sucesso!");
+		
+	}
+	
+	public void listarClientes() {
+
+	    for(Cliente c: clientes) {
+	        c.exibirCliente();
+	    }
+
+	}
+	
+	public void buscarClientePorId(int id) {
+
+		for(Cliente c: clientes) {
+
+	        if(c.getId() == id) {
+	            c.exibirCliente();
+	            return;
+	        }
+
+	    }
+
+	    System.out.println("Cliente não encontrado");
+
+	}
+	
+	public void removerClientePorId(int id) {
+
+		for(Cliente c: clientes) {
+
+	        if(c.getId() == id) {
+
+	            clientes.remove(c);
+
+	            System.out.println("Cliente removido");
+	            return;
+	        }
+
+	    }
+
+	    System.out.println("Cliente não encontrado");
+
+	}
+
+}
